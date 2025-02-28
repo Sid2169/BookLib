726426d1c9413319169acf34bd0b3aab8475c8bc
Sid2169 <siddharthasingh21109572@gmail.com>
2025-03-01 00:22:56 +0530

    Implement addition/deletion of books -renamed variable 'container' to 'formContainer' -implement form submission -implement read toggle button -implement cofirmation popup

--- script.js
+++ script.js
@@ -1,9 +1,109 @@
 // Add New+ button functionality
 const translucentOverlay = document.querySelector('.translucent-overlay');
-const form = document.querySelector('.form-container');
+const formContainer = document.querySelector('.form-container');
 const addNewBtn = document.getElementById('addBook');
 
 addNewBtn.addEventListener('click', () => {
-    form.classList.remove('hidden');
+    formContainer.classList.remove('hidden');
     translucentOverlay.classList.remove('hidden');
+});
+
+//SUBMIT NEW BOOK FUNCTIONALITY
+
+const myLibrary = []; //Array for my library
+
+//Book object constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    //generate a unique bookId
+    this.bookId = `${title.replace(/\s+/g, "_")}-${author.replace(/\s+/g, "_")}-${pages}-${Date.now()}`;
+}
+
+//Add book to library function
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+const cardContainer = document.querySelector('.container');
+
+//Functionality for cancel delete button of delete confirmation popup
+const popup = document.querySelector('.popup-content');
+const cancelDelBtn = document.getElementById('cancelDelete');
+cancelDelBtn.addEventListener('click', () => {
+    translucentOverlay.classList.add('hidden');
+    popup.classList.add('hidden');
+})
+
+//Functionality for form submit action
+const bookForm = document.getElementById('bookForm');
+
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault() //prevents page refresh
+
+    //Propertiy values for generaing new book object 
+    let title = document.getElementById('bookTitle').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('readCheckbox').checked;
+
+    addBookToLibrary(title, author, pages, read);//Add new book object to myLibrary array
+    const newlyGeneratedBook = myLibrary[myLibrary.length - 1];
+
+    //Generate New Card for the newly added book in myLibrary
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
+    const newCardId = newlyGeneratedBook.bookId;
+    newCard.id = newCardId;
+    newCard.innerHTML =
+        `   <div class="title">${title}</div>
+            <div class="author">Author :${author}</div>
+            <div class="pages">Total Pages : ${pages}</div>
+            <button class="read tool-tip" data-tooltip="Read Book Toggle"></button>
+            <button class="delete tool-tip" data-tooltip="Delete-Book"></button>`
+
+    cardContainer.appendChild(newCard);//Appending the new card to card container
+
+    //Functionality for readToggle button
+    const readToggle = newCard.querySelector('.read');
+    if (newlyGeneratedBook.read) {
+        readToggle.classList.add('toggled');
+    }
+
+    readToggle.addEventListener('click', () => {
+        if (!newlyGeneratedBook.read) {
+            newlyGeneratedBook.read = true;
+            readToggle.classList.add('toggled');
+        }
+        else {
+            newlyGeneratedBook.read = false;
+            readToggle.classList.remove('toggled');
+        }
+    });
+
+    //Functionality for delete book button
+    const deleteBtn = newCard.querySelector(`.delete`);
+    deleteBtn.addEventListener('click', () => {
+        let index = myLibrary.findIndex(book => book.bookId === newCardId);
+        //Show delete confirmation popup
+        translucentOverlay.classList.remove('hidden');
+        popup.classList.remove('hidden');
+        //Confirm delete button functionality
+        const cnfrmDelBtn = document.getElementById('confirmDelete');
+        cnfrmDelBtn.addEventListener('click', () => {
+            myLibrary.splice(index, 1);
+            newCard.remove();
+            translucentOverlay.classList.add('hidden');
+            popup.classList.add('hidden');
+        });
+    });
+
+    //Reset and hide form after submission
+    bookForm.reset();
+    formContainer.classList.add('hidden');
+    translucentOverlay.classList.add('hidden');
 });
